4ddbf12f3f30086ab31c918cdf2937944cf180fb
Hasan9955 <hasanbinali7556@gmail.com>
2023-10-21 09:52:57 +0600

    remove-env

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 
-const app = express() 
+const app = express()
 const port = process.env.PORT || 5000
 
 
 // middleware 
 app.use(cors())
 app.use(express.json())
+
+
 
 
 // mongo db setup!!!
@@ -30,12 +32,46 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const brandCollection = client.db("BrandDB").collection("brand")
+    const productCollection = client.db('ProductDB').collection("Product")
+
+
+    app.get('/brands', async (req, res) => {
+      const cursor = brandCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    app.get('/products/:name', async (req, res) => {
+      const brandName = req.params.name
+      const query = { brand: req.params.name }
+      const result = productCollection.find(query)
+      const product = await result.toArray()
+      res.send(product)
+    })
+
+
+
+
+    app.post('/products', async (req, res) => {
+      const product = req.body
+      const result = await productCollection.insertOne(product)
+      res.send(result)
+    })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -45,9 +81,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('SERVER IS RUNNING !!!')
+  res.send('SERVER IS RUNNING !!!')
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+  console.log(`Server is running on port: ${port}`)
 })
